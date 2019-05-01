0a64a10d1f3a422fa065700055e6f93115fe19e8
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-05-01 16:52:27 +0200

    Implemented socket.io, and added more middleware for sessions

--- app.js
+++ app.js
@@ -1,23 +1,49 @@
 "use strict";
 
+///////////////
+//  Modules  //
+///////////////
+
 const express = require("express"),
       session = require("express-session"),
       socketIO = require("socket.io"),
       http = require("http"),
       ejs = require("ejs");
 
+/////////////////
+//  Constants  //
+/////////////////
+
 const port = 5000;
 
-const app = express()
-const server = http.Server(app);
-const io = socketIO(server);
+//////////////////
+//  Middleware  //
+//////////////////
+
+const app = express(),
+      server = http.Server(app),
+      io = socketIO(server);
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 app.use(express.static("public"))
+app.use(session({secret: "classified"}));
+
+///////////////
+//  Routing  //
+///////////////
 
 app.get("/", (req, res) => {
   res.render("lobby.ejs")
 })
+
+///////////////
+//  Sockets  //
+///////////////
+
+io.on("connection", socket => {
+
+})
+
 
 server.listen(port, () => console.log(`Listening to port: ${port}`));
