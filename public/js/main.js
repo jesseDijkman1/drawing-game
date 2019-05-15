0bbe275b2f2bc7736a122605b972fd5a2659f9e9
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-05-15 12:47:33 +0200

    Can now change the color of the lines, (I tried to experiment with extends, set and get)

--- public/js/main.js
+++ public/js/main.js
@@ -5,23 +5,27 @@ const socket = io();
 const canvas = document.querySelector("canvas");
 const chat = document.querySelector("#room-chat .chat-messages");
 
+const penColor = document.querySelector(".pen-option-color");
+const penSize = document.querySelector(".pen-option-size");
+
 (() => {
   canvas.setAttribute("width", canvas.offsetWidth)
   canvas.setAttribute("height", canvas.offsetHeight)
 })()
 
-class Drawing {
-  constructor(target) {
-    this.ctx = target.getContext("2d");
-    this.points = [];
+class Game {
+  constructor(c) {
+    this.ctx = c.getContext("2d");
+    this.drawings;
   }
 
-  addPoint(point) {
-    this.points.push(point)
-  }
+  render(drawing) {
 
-  render() {
-    this.points.forEach((p, i, a) => {
+    this.ctx.strokeStyle = drawing.drawingColor;
+    this.ctx.lineJoin = "round";
+    // this.ctx.lineWidth = 100;
+
+    drawing.points.forEach((p, i, a) => {
       this.ctx.beginPath();
 
       if (i == 0) {
@@ -37,30 +41,71 @@ class Drawing {
   }
 }
 
+const game = new Game(canvas)
+
+class PenSettings {
+  constructor(colorsContainer, sizeContainer) {
+    this.colorInputs = colorsContainer.querySelectorAll("input");
+    this.sizeContainer = sizeContainer.childElements;
+    this.penColor;
+
+  }
+
+  get color() {
+    this.color = this.colorInputs
+
+    return this.penColor;
+  }
+
+  set color(inputs) {
+    const data = {}
+
+    for (let i = 0; i < inputs.length; i++) {
+      data[inputs[i].dataset.char] = `${inputs[i].value}${inputs[i].dataset.unit || ""}`
+    }
+
+    this.penColor = `${Object.keys(data).join("")}(${Object.values(data).join(",")})`
+  }
+}
+
+class Drawing extends PenSettings {
+  constructor(target, penColor, penSize) {
+    super(penColor, penSize)
+
+    this.drawingColor = super.color;
+    this.points = [];
+  }
+
+  addPoint(point) {
+    this.points.push(point)
+  }
+}
+
 class Point {
-  constructor(e) {
-    this.x = e.clientX - e.target.offsetLeft;
-    this.y = e.clientY - e.target.offsetTop;
+  constructor(e, target) {
+    this.x = e.clientX - target.offsetLeft;
+    this.y = e.clientY - target.offsetTop;
   }
 }
 
 function startDrawing(e) {
   const startX = e.clientX,
         startY = e.clientY,
-        drawing = new Drawing(e.currentTarget);
+        drawing = new Drawing(e.currentTarget, penColor, penSize);
 
   window.addEventListener("mousemove", isDrawing)
   window.addEventListener("mouseup", endDrawing)
-  
+
   e.target.addEventListener("mouseleave", endDrawing)
 
   function isDrawing(ev) {
-    drawing.addPoint(new Point(ev))
-    drawing.render()
+    drawing.addPoint(new Point(ev, ev.target.parentElement))
+
+    game.render(drawing)
   }
 
   function endDrawing(ev) {
-    drawing.render()
+    // drawing.render()
 
     window.removeEventListener("mousemove", isDrawing)
   }
